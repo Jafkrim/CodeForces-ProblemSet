dd9ad03b647a58be2cdc24d2a3001497d0907803
Jaf_Alkarim <149105089+Jafkrim@users.noreply.github.com>
2024-10-29 12:05:51 +0700

    Create cf_791A.c
    third

---
+++ C/Solved (Z-A)/cf_791A.c
@@ -0,0 +1,24 @@
+/*
+    int a, b -> 1 <= a && a <= b && b <= 10
+    int num_year --> Result of years after which Limak will become strictly larger than Bob
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+// https://github.com/Jafkrim
+int main() {
+    int a, b, num_year = 0;
+
+    scanf("%d %d", &a, &b);
+
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        num_year++;
+    }
+
+    printf("%d\n", num_year);
+
+    return 0;
+}
